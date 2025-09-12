1a2a2d528d4ab0c6bf15bbabec2b0138a7d3a0a1
CongDat <dungdelaptk3@gmail.com>
2025-09-12 10:01:22 +0700

    pw4 add viewtext and viewimage

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -8,16 +8,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ForecastFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup containter, Bundle savedInstanceState) {
 
-        View view = new View(getActivity());
-        int color = 0x20FF0000;
-        view.setBackgroundColor(color);
-        return view;
+        LinearLayout linearLayout = new LinearLayout(getActivity());
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        TextView thurs = new TextView(getActivity());
+        thurs.setText("Thursday");
+
+        ImageView weatherIcon = new ImageView(getActivity());
+        weatherIcon.setImageResource(R.drawable.cloudy);
+
+        linearLayout.setBackgroundColor(0x20FF0000);
+
+        linearLayout.addView(thurs);
+        linearLayout.addView(weatherIcon);
+
+        return linearLayout;
     }
 
 }
