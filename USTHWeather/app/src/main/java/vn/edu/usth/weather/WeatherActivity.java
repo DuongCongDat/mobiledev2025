76a60e0f79118de1d26c1cba978480c639125a79
CongDat <dungdelaptk3@gmail.com>
2025-09-15 12:06:46 +0700

    pw6

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -14,48 +14,5 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-
-        if (savedInstanceState == null) {
-            ForecastFragment firstFragment = new ForecastFragment();
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.add(R.id.container, firstFragment);
-            ft.commit();
-
         }
     }
-//
-//    @Override
-//    protected void onStart(){
-//        super.onStart();
-//        Log.i(WeaAc,"****Start****");
-//    }
-//
-//    @Override
-//    protected void onResume(){
-//        super.onResume();
-//        Log.i(WeaAc,"****Resume****");
-//    }
-//
-//    @Override
-//    protected void onPause(){
-//        super.onPause();
-//        Log.i(WeaAc,"****Pause****");
-//    }
-//
-//    @Override
-//    protected void onStop(){
-//        super.onStop();
-//        Log.i(WeaAc,"****Stop****");
-//    }
-//    @Override
-//    protected void onDestroy(){
-//        super.onDestroy();
-//        Log.i(WeaAc,"****Destroy****");
-//    }
-}
-//startapp: onStart
-//back: onPause, onStop, onDestroy
-//home: onPause, onStop
-//recent: onPause, onStop
-//tap app at recent mode: onStart, onResume
-//kill app: onPaseu, onStop, onDestroy
